4aa5f7aaa82a61e3543a042acde6f870df2443fe
miguelmundi <miguel.mundi.aragones@kairosds.com>
2025-01-15 10:20:25 +0100

    create users controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
 
-@Module({})
+@Module({
+  imports: [],
+  controllers: [UsersController],
+  providers: []
+})
 export class UsersModule {}
